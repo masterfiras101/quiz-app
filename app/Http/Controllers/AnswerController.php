dac80c0399fb4e59d152c6db227a4e2e5daa14cf
programmer <fmod400@gmail.com>
2024-09-15 02:57:27 +0300

    update-answer-controller

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -7,62 +7,13 @@ use Illuminate\Http\Request;
 
 class AnswerController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Answer $answer)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Answer $answer)
-    {
-        //
-    }
+  
 
     /**
      * Update the specified resource in storage.
      */
     public function update(Request $request, Answer $answer)
     {
-        
-        // return $request->all();
-        // $answers = $request->all();
-        // foreach ($answers as $a) {
-
-        //     $id = $a['id'];
-        //     $updateAnswer = Answer::findOrFail($id);
-        //     $updateAnswer->answer=$a['answer'];
-        //     $updateAnswer->correct_answer=$a['correct_answer'];
-        //     $updateAnswer->save();
-        // }
 
         foreach ($request->all() as $ans_wer) {
             Answer::where('id', $ans_wer['id'])->update([
@@ -76,11 +27,4 @@ class AnswerController extends Controller
 
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Answer $answer)
-    {
-        //
-    }
 }
