b27ae120ef7b43ea813d874f815408696dddcd59
programmer <fmod400@gmail.com>
2024-09-29 21:49:37 +0300

    add jsonresource in boot function

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Http\Resources\Json\JsonResource;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -20,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Vite::prefetch(concurrency: 3);
+        // Vite::prefetch(concurrency: 3);
+        JsonResource::withoutWrapping();
+
     }
 }
